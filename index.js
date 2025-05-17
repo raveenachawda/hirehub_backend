e758f9709dfcfce17a00afe815ee687770b1c6e5
raveenachawda <raveenachawda0228@gmail.com>
2025-05-17 17:57:48 +0530

    change fronend url
    change frontend url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["https://hirehub-frontend-blond.vercel.app", "http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
